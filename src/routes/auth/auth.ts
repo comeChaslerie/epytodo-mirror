0b89c69c6fee68a114ba9cd55836c59045830439
comeChaslerie <come.chaslerie@epitech.eu>
2026-05-13 09:20:51 +0200

    <modify>(auth.ts):improve code readibility, and error msg, add log route

--- src/routes/auth/auth.ts
+++ src/routes/auth/auth.ts
@@ -12,16 +12,40 @@ router.post("/register", async (req, res) => {
   try {
     const { email, password, name, firstname } = req.body;
     if (!email || !password || !name || !firstname)
-      return res.status(404).json({ msg: "Incorrect arg\n" });
+      return res.status(404).json({ msg: "Bad parameter" });
+
     const already_exist = await findUserByEmail(email);
     if (already_exist)
-      return res.status(404).json({ msg: "User already exist\n" });
+      return res.status(404).json({ msg: "Bad parameter" });
+
     const hashed = await bcrypt.hash(password, 10);
+
     const id = await createUser(email, hashed, name, firstname);
+
     const token = jwt.sign({ id }, process.env.SECRET!, { expiresIn: "1h" });
     return res.json({ token });
   } catch (err) {
-    return res.status(500).json({ msg: "Internal server error\n" });
+    return res.status(500).json({ msg: "Internal server error" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password)
+      return res.status(404).json({ msg: "Bad parameter" });
+
+    const user = await findUserByEmail(email);
+    if (!user)
+      return res.status(404).json({ msg: "Bad parameter" });
+
+    if (!bcrypt.compare(password, user.password))
+      return res.status(404).json({ msg: "Bad parameter" });
+
+    const token = jwt.sign( user.id, process.env.SECRET!, { expiresIn: "1h" });
+    return res.json({ token });
+  } catch (err) {
+    return res.status(500).json({ msg: "Internal server error" });
   }
 });
 
