0f9331c4517a13f0437ee13cd3c98894635ae7f5
Stan Gaumain <stanislas.gaumain@epitech.eu>
2026-05-13 02:35:40 +0200

    add(index) : open the server and create the route from different files

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import authRouter from "./routes/auth/auth";
+import userRouter from "./routes/user/user";
+import todosRouter from "./routes/todos/todo";
+
+
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
+
+app.use(authRouter);
+app.use(userRouter);
+app.use(todosRouter);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
